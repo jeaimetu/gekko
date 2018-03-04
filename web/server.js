ee9a94f99ee5e77409c56096257fc60559ebe0b4
jeaimetu <32817067+jeaimetu@users.noreply.github.com>
2018-03-04 23:54:19 +0900

    Update server.js

--- web/server.js
+++ web/server.js
@@ -75,9 +75,9 @@ app
   .use(router.routes())
   .use(router.allowedMethods());
 
-server.timeout = config.api.timeout||120000;
-server.on('request', app.callback());
-server.listen(config.api.port, config.api.host, '::', () => {
+//server.timeout = config.api.timeout||120000;
+//server.on('request', app.callback());
+server.listen(config.api.port, () => {
   const host = `${config.ui.host}:${config.ui.port}${config.ui.path}`;
 
   if(config.ui.ssl) {
