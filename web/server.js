d689de8e962532303abb642698dde70324b618e4
jeaimetu <32817067+jeaimetu@users.noreply.github.com>
2018-03-05 11:43:15 +0900

    Update server.js

--- web/server.js
+++ web/server.js
@@ -78,8 +78,9 @@ app
 server.timeout = config.api.timeout||120000;
 server.on('request', app.callback());
 //server.listen(config.api.port, config.api.host, '::', () => {
-server.listen(process.env.PORT, process.env.HOST, '::', () => {
-  const host = `${process.env.HOST}:${process.env.PORT}${config.ui.path}`;
+config.api.host = "murmuring-dawn-73902.herokuapp.com";
+server.listen(process.env.PORT, config.api.host, '::', () => {
+  const host = `${config.api.host}:${process.env.PORT}${config.ui.path}`;
 
   if(config.ui.ssl) {
     var location = `https://${host}`;
