f020dacd8872027b354834a3e856355c1f552ce6
jeaimetu <32817067+jeaimetu@users.noreply.github.com>
2018-03-06 17:17:53 +0900

    Update server.js

--- web/server.js
+++ web/server.js
@@ -80,7 +80,8 @@ server.timeout = config.api.timeout||120000;
 server.on('request', app.callback());
 //server.listen(config.api.port, config.api.host, '::', () => {
 server.listen(process.env.PORT, () => {
-  const host = `${config.api.host}:${process.env.PORT}${config.ui.path}`;
+  //const host = `${config.api.host}:${process.env.PORT}${config.ui.path}`;
+  const host = `${process.env.DOMAIN}${config.ui.path}`;
 
   if(config.ui.ssl) {
     var location = `https://${host}`;
