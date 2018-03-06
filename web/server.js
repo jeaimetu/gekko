61a40550a19c3c7cc6658ef1c14173ff1af24575
jeaimetu <32817067+jeaimetu@users.noreply.github.com>
2018-03-06 17:46:48 +0900

    Update server.js

--- web/server.js
+++ web/server.js
@@ -78,8 +78,8 @@ app
 
 server.timeout = config.api.timeout||120000;
 server.on('request', app.callback());
-server.listen(config.api.port, config.api.host, '::', () => {
-//server.listen(process.env.PORT, () => {
+//server.listen(config.api.port, config.api.host, '::', () => {
+server.listen(process.env.PORT, () => {
   const host = `${config.ui.host}:${process.env.PORT}${config.ui.path}`;
   //const host = `${process.env.DOMAIN}${config.ui.path}`;
   console.log('process env',process.env);
